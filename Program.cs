52e72d55d18ff096a7d114077af3cda9b7d33475
Benjamin <Benjamin-S@users.noreply.github.com>
2019-04-20 17:49:19 +0800

    Larger Datasource Grabbed

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using DataGrab.Models;
+using DataGrab.Models.DS2;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -15,17 +17,36 @@ namespace DataGrab
         static void Main(string[] args)
         {
             var jsonURL = @"https://data.melbourne.vic.gov.au/resource/imwx-szwr.json?$order=:id&$limit=100000&$offset=0";
-            var geoString = GetJsonData(jsonURL);
+            var ds2URL = @"https://data.melbourne.vic.gov.au/resource/44kh-ty54.json?$order=:id&$limit=100000&$offset=0";
+            //var geoString = GetJsonData(jsonURL);
+            // var geoString2 = GetJsonData(ds2URL);
 
             // Data source doesn't have post codes so I've ripped some from the web
             // and will insert into each record
             var zipString = GetZipData(@"Data\postalAreas.json");
 
-            // Map to intermediary model for renaming and discarding of properties
-            var addresses = Address.FromJson(geoString);
-            List<AddressDTO> addressList = MapToDTO(addresses, zipString);
+            // // Map to intermediary model for renaming and discarding of properties
+            // var addresses = Address.FromJson(geoString);
+            // List<AddressDTO> addressList = MapToDTO(addresses, zipString);
 
-            using (StreamWriter outFile = File.CreateText(@"Data\AddressDTO.json"))
+            // using (StreamWriter outFile = File.CreateText(@"Data\AddressDTO.json"))
+            // {
+            //     JsonSerializer serializer = new JsonSerializer();
+            //     serializer.Serialize(outFile, addressList);
+            // }
+
+            string geoString2;
+
+            using (StreamReader streamReader = new StreamReader(@"Data\44kh-ty54.json", Encoding.UTF8))
+            {
+                geoString2 = streamReader.ReadToEnd();
+            }
+
+            // Map to intermediary model for renaming and discarding of properties
+            var addresses = AddressDS2.FromJson(geoString2);
+            List<AddressDTO> addressList = MapToDTO2(addresses, zipString);
+
+            using (StreamWriter outFile = File.CreateText(@"Data\AddressDTO2.json"))
             {
                 JsonSerializer serializer = new JsonSerializer();
                 serializer.Serialize(outFile, addressList);
@@ -76,6 +97,35 @@ namespace DataGrab
                     tempDTO.Zip = GetZip(ZipString, address.Suburb.ToUpper());
                     tempDTO.Latitude = Convert.ToDouble(address.Latitude);
                     tempDTO.Longitude = Convert.ToDouble(address.Longitude);
+
+                    addressDTO.Add(tempDTO);
+                }
+            }
+
+            return addressDTO;
+        }
+
+
+        static List<AddressDTO> MapToDTO2(List<AddressDS2> addresses, string ZipString)
+        {
+            List<AddressDTO> addressDTO = new List<AddressDTO>();
+
+            foreach (var address in addresses)
+            {
+                // Theres only 6 addresses that lack a suburb
+                if (address.ClueSmallArea != null)
+                {
+                    AddressDTO tempDTO = new AddressDTO();
+
+                    StringBuilder sb = new StringBuilder();
+
+                    tempDTO.City = address.ClueSmallArea;
+                    tempDTO.Street = address.StreetName;
+                    tempDTO.State = "Victoria";
+                    var tempSuburb = Regex.Replace(address.ClueSmallArea, @" \((.*?)\)", "");
+                    tempDTO.Zip = GetZip(ZipString, tempSuburb.ToUpper());
+                    tempDTO.Latitude = Convert.ToDouble(address.YCoordinate);
+                    tempDTO.Longitude = Convert.ToDouble(address.XCoordinate);
 
                     addressDTO.Add(tempDTO);
                 }
